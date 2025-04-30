dfef444641e597c9b2fd7d4f2f713d20af51b4ca
midicas <casbouwman02@gmail.com>
2025-04-30 13:02:30 +0200

    Added a base model that we can use for testing different parts of the infrastructure

--- models.py
+++ models.py
@@ -1,12 +1,33 @@
 import torch
 import torch.nn as nn
 
+class testModel(torch.nn.Module):
+    """
+    Base model invoked for testing training functions
+    """
+    def __init__(self,n):
+        self.linear = torch.nn.Linear(n,1)
+        self.activation = torch.nn.ReLU()
+    def forward(self,x):
+        return self.activation(self.linear(x))
+    
 class MLP(torch.nn.Module):
     def __init__(self,n):
         super(MLP,self).__init__()
         self.linear = torch.nn.Linear(n,200)
         self.activation = torch.nn.ReLU()
-        self.linear2 = torch.nn.Linear(200,1)
-    def forward(x):
-        out = linear(x)
-        out = activation
+        self.linear2 = torch.nn.Linear(200,100)
+        self.linear3 = torch.nn.Linear(100,1)
+    
+    def forward(self,x):
+        out = self.linear(x)
+        out = self.activation(out)
+        out = self.linear2(out)
+        out = self.linear3(out)
+
+class RNN(torch.nn.Module):
+    def __init__(self,n):
+        super(RNN,self).__init__()
+        self.linear = torch.nn.Linear(n,200)
+        self.activation = torch.nn.ReLU()
+
