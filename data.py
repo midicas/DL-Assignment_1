ee2694f0701890c01acef2d5bce9083813821170
Wessel van Gils <wvgils2003@gmail.com>
2025-04-30 17:15:50 +0200

    data loads into a torch dataset

--- data.py
+++ data.py
@@ -1,9 +1,30 @@
-def data(fname):
-    features,targets = None
-    return features,targets
+from scipy.io import loadmat
 
-def training(model,data):
-    model_results = None
-    return model,model_results
+raw = loadmat("Xtrain.mat")["Xtrain"].squeeze()
 
-def 
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+class Dataset(Dataset):
+    def __init__(self, data, seq_length=3):
+        self.data = torch.tensor(data, dtype=torch.uint8)
+        self.seq_length = seq_length
+
+    def __len__(self):
+        return len(self.data) - self.seq_length
+
+    def __getitem__(self, idx):
+        x = self.data[idx : idx + self.seq_length]
+        y = self.data[idx + self.seq_length]
+        return x, y
+
+
+dataset = Dataset(raw, seq_length=5)
+loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+# Print the first batch
+for x_batch, y_batch in loader:
+    print("Input sequence (x):", x_batch)
+    print("Target (y):", y_batch)
+    break
