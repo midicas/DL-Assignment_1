ab1c6a4f6e53c3d63cdd8cd7f8d9de6360144807
midicas <95248199+midicas@users.noreply.github.com>
2025-05-06 15:22:59 +0200

    intermediate changes

--- data.py
+++ data.py
@@ -1,7 +1,5 @@
 from scipy.io import loadmat
 
-raw = loadmat("Xtrain.mat")["Xtrain"].squeeze()
-
 import torch
 from torch.utils.data import Dataset, DataLoader
 
@@ -19,21 +17,29 @@ class LaserDataset(Dataset):
         y = self.data[idx + self.seq_length]
         return x, y
 
+def createData(fname,seq_length):
+    raw = loadmat("Xtrain.mat")["Xtrain"].squeeze()
+    
+    dataset = LaserDataset(raw, seq_length=5)
+    
+    loader = DataLoader(dataset, batch_size=2, shuffle=False)
 
-dataset = LaserDataset(raw, seq_length=5)
-loader = DataLoader(dataset, batch_size=32, shuffle=True)
-
+    return loader
 # Example usage
-from models import testModel
 
-model = testModel(n=5)
+if __name__ == "__main__":
+    from models import TransformerWithPE
 
-for x_batch, y_batch in loader:
-    output = model(x_batch.float())
-    print("Input batch (x):")
-    print(x_batch)
-    print("Model output:")
-    print(output)
-    print("Target (y):")
-    print(y_batch)
-    break 
+    model = TransformerWithPE(n=5)
+    
+    loader = createData("Xtrain.mat",5)
+
+    for x_batch, y_batch in loader:
+        output = model(x_batch.float())
+        print("Input batch (x):")
+        print(x_batch)
+        print("Model output:")
+        print(output)
+        print("Target (y):")
+        print(y_batch)
+        break 
